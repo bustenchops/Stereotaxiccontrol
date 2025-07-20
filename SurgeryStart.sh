e7147556805ed11efe3dc936d53bbbd3415820fe
bustenchops <mmmm_donut@msn.com>
2025-07-20 00:07:52 -0600

    almost done

--- SurgeryStart.sh
+++ SurgeryStart.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-rclone copy onedrive:'Usask Job/Targetlists' /home/bustenchops/Stereotaxiccontrol/TargetLists
+rclone copy onedrive:'Usask Job/Targetlists' /home/bustenchops/TargetLists
 
 cd /home/bustenchops/Stereotaxiccontrol
 
 git pull
+
+python StereotaxicUI.py
