8525d931996a92433c5ff054c43423768396f466
bustenchops <mmmm_donut@msn.com>
2025-11-23 19:35:06 -0600

    I made changes in to the print lines to make them more clear...if something doesn't work go back one commit.

--- LeftHand/SurgeryStart.sh
+++ LeftHand/SurgeryStart.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+cd ..
 
-rclone copy -P onedrive:'Usask Job/Targetlists' /home/bustenchops/TargetLists
+source ./venv1/bin/activate
 
-cd /home/bustenchops/Stereotaxiccontrol
+rclone copy -P onedrive:'fulldirectory/finalfolder' /home/rest_of_directory_to/TargetLists
+
+cd /home/rest_of_directory_to/Stereotaxiccontrol
 
 git pull
 
+# Pick one and uncomment it
+# cd /RightHand
+# cd /LeftHand
+
 python StereotaxicUI.py
+
+read -p "press enter to exit..."
