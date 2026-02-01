11388d6b45d5d9e6cde3a8f21782097e0cc5d37f
bustenchops <30851972+bustenchops@users.noreply.github.com>
2026-01-31 21:00:31 -0600

    Add files via upload
    31jan26

---
+++ debouncecalculator.py
@@ -0,0 +1,7 @@
+# Two-resistor debounce time calculator
+R1 = 10000    # ohms series resistor
+R2 = 100000      # ohms pullup resistor
+C  = 1e-6       # farads
+
+t_ms = (R1 * R2 / (R1 + R2)) * C * 1000
+print(f"Debounce time: {t_ms:.2f} ms")
