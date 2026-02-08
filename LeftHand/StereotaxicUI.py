0108469bb55ee80f1598c4fa9af51ce0fc204a6e
bustenchops <30851972+bustenchops@users.noreply.github.com>
2026-02-08 12:42:32 -0600

    screen resize

--- LeftHand/StereotaxicUI.py
+++ LeftHand/StereotaxicUI.py
@@ -195,7 +195,7 @@ class MainWindow(QMainWindow):
 
         self.targetname = QPlainTextEdit(self.widget)
         self.targetname.setObjectName(u"targetnameselected")
-        self.targetname.setGeometry(QRect(500, 300, 300, 35))
+        self.targetname.setGeometry(QRect(500, 298, 275, 33))
         self.targetname.setFont(radiobuttonfont)
 
 
