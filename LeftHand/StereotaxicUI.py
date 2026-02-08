bab4fa48ab6d0929d28787da0e9f1eb4e3751b70
bustenchops <30851972+bustenchops@users.noreply.github.com>
2026-02-08 00:21:26 -0600

    screen resize

--- LeftHand/StereotaxicUI.py
+++ LeftHand/StereotaxicUI.py
@@ -137,19 +137,19 @@ class MainWindow(QMainWindow):
 
         self.stepposlabel = QLabel("Step:", self.widget)
         self.stepposlabel.setObjectName(u"stepposlabel")
-        self.stepposlabel.setGeometry(QRect(40, 37, 111, 41))
+        self.stepposlabel.setGeometry(QRect(15, 37, 111, 41))
         self.stepposlabel.setFont(poslabelfont)
 
 
 
         self.ABSposLabel = QLabel("ABS:", self.widget)
         self.ABSposLabel.setObjectName(u"ABSposlabel")
-        self.ABSposLabel.setGeometry(QRect(40, 92, 111, 61))
+        self.ABSposLabel.setGeometry(QRect(15, 92, 111, 61))
         self.ABSposLabel.setFont(poslabelfont)
 
         self.RELposLabel = QLabel("REL:", self.widget)
         self.RELposLabel.setObjectName(u"RELposlabel")
-        self.RELposLabel.setGeometry(QRect(40, 152, 111, 61))
+        self.RELposLabel.setGeometry(QRect(15, 152, 111, 61))
         self.RELposLabel.setFont(poslabelfont)
 
         manualenterfont = QFont()
