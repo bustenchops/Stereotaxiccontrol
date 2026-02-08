d1e0d8f8da836df7bc0906845082d1bc60720349
bustenchops <30851972+bustenchops@users.noreply.github.com>
2026-02-08 17:06:53 -0600

    screen resize

--- LeftHand/StereotaxicUI.py
+++ LeftHand/StereotaxicUI.py
@@ -232,18 +232,18 @@ class MainWindow(QMainWindow):
         self.DVinsertratelabel.setFont(smalllabelfont)
 
         self.DVinsertpauselabel = QLabel("Pause time:", self)
-        self.DVinsertpauselabel.setObjectName(u"dvnumberpauses")
-        self.DVinsertpauselabel.setGeometry(QRect(701, 422, 130, 20))
+        self.DVinsertpauselabel.setObjectName(u"dvpausetime")
+        self.DVinsertpauselabel.setGeometry(QRect(701, 420, 130, 20))
         self.DVinsertpauselabel.setFont(smalllabelfont)
 
         self.DVinsertpausetimelabel = QLabel("Num. pauses:", self)
-        self.DVinsertpausetimelabel.setObjectName(u"dvinsertpausetime")
+        self.DVinsertpausetimelabel.setObjectName(u"dvnumpauses")
         self.DVinsertpausetimelabel.setGeometry(QRect(701, 365, 130, 20))
         self.DVinsertpausetimelabel.setFont(smalllabelfont)
 
         self.DVinsertdepthlabel = QLabel("Target DV:", self)
         self.DVinsertdepthlabel.setObjectName(u"dvtargetdepth")
-        self.DVinsertdepthlabel.setGeometry(QRect(615, 422, 130, 20))
+        self.DVinsertdepthlabel.setGeometry(QRect(615, 420, 130, 20))
         self.DVinsertdepthlabel.setFont(smalllabelfont)
 
         self.DVinsertmanrate = QPlainTextEdit(self.widget)
@@ -253,7 +253,7 @@ class MainWindow(QMainWindow):
 
         self.DVinserttarget = QPlainTextEdit(self.widget)
         self.DVinserttarget.setObjectName(u"DVinserttarget")
-        self.DVinserttarget.setGeometry(QRect(627, 442, 50, 31))
+        self.DVinserttarget.setGeometry(QRect(627, 440, 50, 31))
         self.DVinserttarget.setFont(smalllmanualfont)
 
         self.DVinsertnumpause = QPlainTextEdit(self.widget)
@@ -263,7 +263,7 @@ class MainWindow(QMainWindow):
 
         self.DVinsertpausetime = QPlainTextEdit(self.widget)
         self.DVinsertpausetime.setObjectName(u"DVinsertpausetime")
-        self.DVinsertpausetime.setGeometry(QRect(711, 442, 50, 31))
+        self.DVinsertpausetime.setGeometry(QRect(711, 440, 50, 31))
         self.DVinsertpausetime.setFont(smalllmanualfont)
 
         self.withdrawcheckbox = QCheckBox("Withdraw:", self.widget)
