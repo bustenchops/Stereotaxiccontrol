58d94b5f99c4ff184c327465f27f16e690762e95
bustenchops <30851972+bustenchops@users.noreply.github.com>
2026-02-08 00:06:18 -0600

    screen resize

--- LeftHand/StereotaxicUI.py
+++ LeftHand/StereotaxicUI.py
@@ -23,7 +23,7 @@ class MainWindow(QMainWindow):
 #Setup GUI LAYOUT
 
         self.setObjectName(u"MainWindow")
-        self.resize(124, 550)
+        self.resize(1240, 550)
 
         self.widget = QWidget()
 
