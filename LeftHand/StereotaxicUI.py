8c0ffd8c1fd7ee36e0f09c31f665aefe7fc17ecf
bustenchops <30851972+bustenchops@users.noreply.github.com>
2026-02-09 00:24:54 -0600

    screen resize

--- LeftHand/StereotaxicUI.py
+++ LeftHand/StereotaxicUI.py
@@ -213,7 +213,7 @@ class MainWindow(QMainWindow):
 
         self.targetlabel = QLabel("Selected Target:", self)
         self.targetlabel.setObjectName(u"selectedtargetlabel")
-        self.targetlabel.setGeometry(QRect(375, 305, 130, 20))
+        self.targetlabel.setGeometry(QRect(375, 305, 200, 20))
         self.targetlabel.setFont(radiobuttonfont)
 
         self.coordinatelabel = QLabel("Coordinates:", self)
