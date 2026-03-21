22bdc3b559d7b56814de0cfb78f6692d95916329
bustenchops <30851972+bustenchops@users.noreply.github.com>
2026-03-20 20:58:15 -0600

    relative zeros ok, speed ok, encoder change

--- LeftHand/Buttonclassv1.py
+++ LeftHand/Buttonclassv1.py
@@ -266,11 +266,11 @@ class buttonprogram:
                         self.ratormouse()
                     elif lastbut[var_list.ratselect] == 1 and lastbut[var_list.mouseselect] == 0:
                         #print('ratmouse=1', lastbut[var_list.ratselect], ' and ratmouse2=0',lastbut[var_list.mouseselect])
-                        var_list.ratormouseselect = 1
+                        var_list.ratormouseselect = 2
                         self.ratormouse()
                     elif lastbut[var_list.ratselect] == 0 and lastbut[var_list.mouseselect] == 1:
                         #print('ratmouse=0', lastbut[var_list.ratselect], ' and ratmouse2=1',lastbut[var_list.mouseselect])
-                        var_list.ratormouseselect = 2
+                        var_list.ratormouseselect = 1
                         self.ratormouse()
                     else:
                         print('species select not working right')
