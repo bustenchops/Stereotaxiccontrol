1ef3a0361baf245ae88a93ac1ea6037921debbf5
bustenchops <30851972+bustenchops@users.noreply.github.com>
2026-06-14 01:25:32 -0600

    syncing Left and Right - if switching to right will need to check the movement and variables.

--- TestingFunction/rotary_classv2goeswithv5.py
+++ TestingFunction/rotary_classv2goeswithv5.py
@@ -72,11 +72,13 @@ class RotaryEncoder:
             self.rev_encodercount += 1
             print('reverse count:', self.rev_encodercount)
         if self.encodercount == 3:
-            self.rev_encodercount = 0
+            # self.rev_encodercount = 0
+            self.encodercount += 1
             print('return true same direction')
             return True
         if self.rev_encodercount == 3:
-            self.encodercount = 0
+            # self.encodercount = 0
+            self.rev_encodercount += 1
                 # and (differencetime) > var_list.backwardrotdelay):
             print('return true opposite direction')
             return True
