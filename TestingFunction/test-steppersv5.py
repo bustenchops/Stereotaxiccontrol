c66c0503faf2cb5e706ce8c35350d9f3752742ca
bustenchops <30851972+bustenchops@users.noreply.github.com>
2026-06-13 21:08:28 -0600

    syncing Left and Right - if switching to right will need to check the movement and variables.

--- TestingFunction/test-steppersv5.py
+++ TestingFunction/test-steppersv5.py
@@ -59,10 +59,10 @@ class mainprogram:
     #DEFINE ROTARY ENCODER PINS (NOTE:  may need to change to match actual)
     rotoA_AP = 25
     rotoB_AP =  8
-    rotoA_ML = 12
-    rotoB_ML = 16
-    rotoA_DV = 20
-    rotoB_DV = 21
+    rotoA_ML = 20
+    rotoB_ML = 21
+    rotoA_DV = 12
+    rotoB_DV = 16
     rotoA_AUX = 14
     rotoB_AUX = 15
 
