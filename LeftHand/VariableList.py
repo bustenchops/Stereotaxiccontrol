6f3d839fa98a0ec0605df0dd40e2dcf03d0235fa
bustenchops <30851972+bustenchops@users.noreply.github.com>
2026-06-14 16:27:22 -0600

    update encoder tests

--- LeftHand/VariableList.py
+++ LeftHand/VariableList.py
@@ -154,7 +154,7 @@ class var_list:
 
 #ENCODER CALC VARIABLES
     eventime = 0
-    eventdelay = 330
+    eventdelay = 400
     backwardrotdelay = 500
     lastdirection = 0 #3 = counterclock, 1 = clock
     thecount = 0
