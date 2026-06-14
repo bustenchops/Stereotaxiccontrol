201351b3aa22d69993a65b9aff7358bb2708abf2
bustenchops <30851972+bustenchops@users.noreply.github.com>
2026-06-14 16:23:17 -0600

    update encoder tests

--- TestingFunction/VariableList.py
+++ TestingFunction/VariableList.py
@@ -198,7 +198,7 @@ class var_list:
 
 #ENCODER CALC VARIABLES
     eventime = 0
-    eventdelay = 330
+    eventdelay = 400
     backwardrotdelay = 500
     lastdirection = 0 #3 = counterclock, 1 = clock
     thecount = 0
